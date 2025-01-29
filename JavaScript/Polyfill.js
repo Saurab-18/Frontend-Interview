aa42187683eadebdecad86cba40565ab30b741ad
Saurab-18 <106486115+Saurab-18@users.noreply.github.com>
2025-01-29 19:09:27 +0530

    Polyfill of Only Once Function

---
+++ JavaScript/Polyfill.js
@@ -0,0 +1,16 @@
+// Polyfill of Only Once Function
+function onlyOnce(fn) {
+  let called = false;
+  return function (...args) {
+    if (!called) {
+      called = true;
+      return fn.apply(this, args);
+    }
+  };
+}
+
+let x = onlyOnce(() => {
+  console.log("Function Runs Only Once");
+});
+x();
+x();
