315184a84811f6661205c1c6683639537e5b6b89
Saurab-18 <106486115+Saurab-18@users.noreply.github.com>
2025-01-29 23:14:12 +0530

    Polyfill of Once and two JS Problems

---
+++ JavaScript/Problems.js
@@ -0,0 +1,26 @@
+// 1. Write a program to remove duplicates from an array ?
+
+// function removeDuplicates(arr) {
+//   let set = new Set();
+//   for (let val of arr) {
+//     set.add(val);
+//   }
+//   console.log(Array.from(set));
+// }
+
+// let array = [2, 3, 5, 7, 3, 1, 9, 7];
+// removeDuplicates(array);
+
+// function removeDuplicates(arr) {
+//   console.log(arr.filter((e, index) => arr.indexOf(e) == index));
+// }
+// let array = [2, 3, 5, 7, 3, 1, 9, 7];
+// removeDuplicates(array);
+
+// 2.  Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.
+
+// function evenNumbers(arr) {
+//   console.log(arr.filter((e) => e % 2 == 0));
+// }
+// let array = [2, 3, 5, 7, 3, 1, 9, 7];
+// evenNumbers(array);
