159e5b446b6182a26068f7d307ab764d8a2011fb
Saurab-18 <106486115+Saurab-18@users.noreply.github.com>
2025-02-05 21:39:33 +0530

    Polyfill of memoize function

--- JavaScript/Polyfill.js
+++ JavaScript/Polyfill.js
@@ -14,3 +14,30 @@
 // });
 // x();
 // x();
+
+// Polyfill of Memoize fuction
+
+// function memoize(cb) {
+//   let cache = new Map();
+//   return function (...args) {
+//     let key = JSON.stringify(args);
+//     if (cache.has(key)) {
+//       return cache.get(key);
+//     }
+//     let result = cb(...args);
+//     cache.set(key, result);
+//     return result;
+//   };
+// }
+
+// function square(num) {
+//   return num * num;
+// }
+
+// let result = memoize(square);
+// console.time("1st");
+// console.log(22222222);
+// console.timeEnd("1st");
+// console.time("2st");
+// console.log(22222222);
+// console.timeEnd("2st");
