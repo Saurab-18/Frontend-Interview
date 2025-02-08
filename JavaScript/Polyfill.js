b6bb7c25d119f7cbaeefe8564f77e566c509e414
Saurab-18 <106486115+Saurab-18@users.noreply.github.com>
2025-02-08 10:58:32 +0530

    Polyfill of Map, Filter and Reduce

--- JavaScript/Polyfill.js
+++ JavaScript/Polyfill.js
@@ -1,4 +1,5 @@
 // Polyfill of Only Once Function
+
 // function onlyOnce(fn) {
 //   let called = false;
 //   return function (...args) {
@@ -34,10 +35,55 @@
 //   return num * num;
 // }
 
-// let result = memoize(square);
+// let memoizeResult = memoize(square);
 // console.time("1st");
 // console.log(22222222);
 // console.timeEnd("1st");
 // console.time("2st");
 // console.log(22222222);
 // console.timeEnd("2st");
+
+// Polyfill of Map function
+
+// Array.prototype.polyMap = function (cb) {
+//   let temp = [];
+//   for (let i = 0; i < this.length; i++) {
+//     temp.push(cb(this[i], i, this));
+//   }
+//   return temp;
+// };
+
+// let arr = [1, 2, 3, 4];
+// let mapResult = arr.polyMap((e) => e * 2);
+// console.log(result);
+
+// Polyfill of Reduce function
+
+// Array.prototype.polyFilter = function (cb) {
+//   let temp = [];
+//   for (let i = 0; i < this.length; i++) {
+//     if (cb(this[i], i, this)) {
+//       temp.push(this[i]);
+//     }
+//   }
+//   return temp;
+// };
+
+// let filterArr = [5, 4, 8, 7, 21];
+// let resultFilter = reduceArr.polyFilter((e) => e > 7);
+// console.log(resultFilter);
+
+// Polyfill of Reduce
+
+// Array.prototype.polyReduce = function (cb, initialValue) {
+//   let acc = initialValue;
+//   for (let i = 0; i < this.length; i++) {
+//     acc = acc ? cb(acc, this[i], i, this) : this[i];
+//   }
+//   return acc;
+// };
+// let reduceArr = [1, 2];
+// let reduceResult = reduceArr.polyReduce((acc, cur) => {
+//   return (acc = acc + cur);
+// }, 0);
+// console.log(reduceResult);
