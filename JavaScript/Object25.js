d85e729af3e6b04e51738b614b405d0671ab2e2e
Saurab-18 <106486115+Saurab-18@users.noreply.github.com>
2025-06-09 21:08:44 +0530

    learnt about react render prop pattern

--- JavaScript/Object25.js
+++ JavaScript/Object25.js
@@ -1,6 +1,6 @@
 // Write a function for deepClone of object
 function deepClone(value) {
-  if (typeof value === null || typeof value !== "object") {
+  if (value === null || typeof value !== "object") {
     return value;
   }
   if (Array.isArray(value)) {
@@ -14,9 +14,8 @@ function deepClone(value) {
 }
 
 // Write a function for shallowClone of object
-
 function shallowClone(value) {
-  if (typeof value === null || typeof value !== "object") {
+  if (value === null || typeof value !== "object") {
     return value;
   }
   if (Array.isArray(value)) {
@@ -24,3 +23,49 @@ function shallowClone(value) {
   }
   return { ...value };
 }
+
+// Write a function to deep merge two objects
+function deepMerge(valA, valB) {
+  if (Array.isArray(valA) && Array.isArray(valB)) {
+    return [...valA, ...valB];
+  }
+  if (isPlainObject(valA) && isPlainObject(valB)) {
+    const newObj = { ...valA };
+    for (let key in valB) {
+      if (Object.prototype.hasOwnProperty.call(valA, key)) {
+        newObj[key] = deepMerge(valA[key], valB[key]);
+      } else {
+        newObj[key] = valB[key];
+      }
+    }
+    return newObj;
+  }
+  return valB;
+}
+
+function isPlainObject(val) {
+  if (val === null) return false;
+  const prototype = Object.getPrototypeOf(val);
+  return prototype === null || prototype == Object.prototype;
+}
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Timeout 1");
+}, 0);
+
+setTimeout(() => {
+  console.log("Timeout 2");
+}, 100);
+
+Promise.resolve()
+  .then(() => {
+    console.log("Promise 1");
+    return Promise.resolve("Promise 2");
+  })
+  .then((res) => {
+    console.log(res);
+  });
+
+console.log("End");
---
+++ React/design-pattern/src/components/RenderPropPattern.js
@@ -0,0 +1,3 @@
+export const RenderPropPattern = ({ render }) => {
+  return render();
+};
